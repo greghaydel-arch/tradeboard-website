355ccabe52e4d6ff23574df0c3343545fb6eb8ba
greghaydel-arch <greghaydel@gmail.com>
2026-06-30 22:15:11 -0500

    Fix: flat repo, remove monorepo deps
    Fix: flat repo, remove monorepo deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite"
+import tailwind from "@tailwindcss/vite";
 import path from "path";
 import runableAnalyticsPlugin from "./vite/plugins/runable-analytics-plugin";
-import honoDevPlugin from "./vite/plugins/hono-dev-plugin";
-
-const root = path.resolve(__dirname, "../..");
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, root, '');
+	const env = loadEnv(mode, __dirname, '');
 	Object.assign(process.env, env);
 
 	return {
-		plugins: [honoDevPlugin(), react(), runableAnalyticsPlugin(), tailwind()],
+		plugins: [react(), runableAnalyticsPlugin(), tailwind()],
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "./src/web"),
@@ -20,8 +17,8 @@ export default defineConfig(({ mode }) => {
 		},
 		server: {
 			allowedHosts: true,
-			hmr: { overlay: false, },
-			cors: false
-		}
+			hmr: { overlay: false },
+			cors: false,
+		},
 	};
 });
